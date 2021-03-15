245ce678a6cf95fccfbe173b49e69231d6ec3946
Tanner Frandsen <tandaman29@yahoo.com>
2021-03-15 10:34:24 -0600

    Resolving lint issues

--- ProjectEuler/problem_007/problem007.py
+++ ProjectEuler/problem_007/problem007.py
@@ -1,5 +1,5 @@
 # 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-#What is the 10001st prime number?
+# What is the 10001st prime number?
 import itertools
 
 
@@ -15,15 +15,16 @@ def find_nth_prime(n):
 def is_prime(number):
     if number <= 1:
         return False
-    elif number == 2: 
+    elif number == 2:
         return True
     elif number % 2 == 0:
         return False
 
-    for x in range(3, int(number** 0.5)+1):
+    for x in range(3, int(number ** 0.5) + 1):
         if number % x == 0:
             return False
     return True
+
 
 if __name__ == '__main__':
     print(f'The 6th prime number is {find_nth_prime(6)}')
